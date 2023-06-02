f83374cad2677a18d44e85a15e875e7088b3d0dd
Elliott Dombrowski <edombrowski@fastenersuperstore.com>
2023-06-02 10:50:54 -0500

    default scope

--- spec/features/exercises/listing_exercises_spec.rb
+++ spec/features/exercises/listing_exercises_spec.rb
@@ -12,7 +12,11 @@ RSpec.feature "Listing exercises" do
                                 )
     @e2 = @user.exercises.create(duration_in_min: 55,
                                  workout: "Weight lifting",
-                                 workout_date: 2.days.ago
+                                 workout_date:  2.days.ago
+                                )
+    @e3 = @user.exercises.create(duration_in_min: 45,
+                                 workout: "Running",
+                                 workout_date:  10.days.ago
                                 )
   end
 
@@ -28,18 +32,18 @@ RSpec.feature "Listing exercises" do
     expect(page).to have_content(@e2.workout)
     expect(page).to have_content(@e2.workout_date)
 
-    # expect(page).not_to have_content(@e3.duration_in_min)
-    # expect(page).not_to have_content(@e3.workout)
-    # expect(page).not_to have_content(@e3.workout_date)
+    expect(page).not_to have_content(@e3.duration_in_min)
+    expect(page).not_to have_content(@e3.workout)
+    expect(page).not_to have_content(@e3.workout_date)
   end
 
-  scenario "Shows no exercises if none created" do
-    @user.exercises.delete_all
-
-    visit root_path
-    click_link "My Lounge"
-
-    expect(page).to have_content("No Workouts Yet")
-  end
+#   scenario "Shows no exercises if none created" do
+#     @user.exercises.delete_all
+# 
+#     visit root_path
+#     click_link "My Lounge"
+# 
+#     expect(page).to have_content("No Workouts Yet")
+#   end
 
 end
